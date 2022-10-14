474113f84133fd26e215f38a1ee6e076b48a9d9d
elftausend <76885970+elftausend@users.noreply.github.com>
2022-10-14 11:39:32 +0200

    Fix impl stack attribute

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-use proc_macro2::Ident;
 use quote::{quote, ToTokens};
-use syn::{parse_macro_input, ItemImpl, TypeParam, Type, punctuated::Punctuated, FnArg, token::Comma, parse_quote::ParseQuote, Signature, PatType, Pat, ReturnType};
+use syn::{parse_macro_input, ItemImpl, Path, ReturnType};
 
 #[proc_macro_attribute]
 pub fn impl_stack(
@@ -11,17 +10,19 @@ pub fn impl_stack(
     proc_macro::TokenStream::from(add_stack_impl(input))
 }
 
-fn add_stack_impl(mut impl_block: ItemImpl) -> proc_macro2::TokenStream {
-    
-    let generics = impl_block.generics.to_token_stream();
-    //let impl_trait = impl_block.trait_.unwrap().2.to_token_stream();
+fn add_stack_impl(impl_block: ItemImpl) -> proc_macro2::TokenStream {
+    let attrs = impl_block.attrs.iter().fold(quote!(), |mut acc, attr| {
+        acc.extend(attr.to_token_stream());
+        acc
+    });
+    let spawn_generics = impl_block.generics.params.to_token_stream();
+    let where_clause = impl_block.generics.where_clause.as_ref().unwrap();
 
     if let Some(generic_type) = impl_block.generics.type_params().next() {
         if generic_type.ident != "T" {
             panic!("--> should use the datatype provided from ...? e.g. #[impl_stack(f32)]");
         }
 
-        let generic_token_stream = generic_type.to_token_stream();
         let impl_trait = &impl_block
             .trait_
             .as_ref()
@@ -31,13 +32,21 @@ fn add_stack_impl(mut impl_block: ItemImpl) -> proc_macro2::TokenStream {
             .to_string();
         let mut path_generics = impl_trait.split('<');
 
-        let trait_name = path_generics.next().unwrap().to_token_stream();
+        let trait_name = path_generics.next().unwrap();
         let generics_no_const = path_generics.next().unwrap();
-        let generics =
-            format!("{}, N", &generics_no_const[..generics_no_const.len() - 1]).to_token_stream();
+        let trait_generics = format!(
+            "{}<{}, N >",
+            trait_name,
+            &generics_no_const[..generics_no_const.len() - 2]
+        );
+
+        let trait_path: Path = syn::parse_str(&trait_generics).unwrap();
+
+        //let generics = remove_lit(generics);
 
         let methods = impl_block
             .items
+            .clone()
             .into_iter()
             .flat_map(|item| match item {
                 syn::ImplItem::Method(method) => Some(method),
@@ -47,48 +56,55 @@ fn add_stack_impl(mut impl_block: ItemImpl) -> proc_macro2::TokenStream {
 
         let methods_updated = methods.into_iter().fold(quote!(), |mut acc, mut meth| {
             if let ReturnType::Type(_, output) = &mut meth.sig.output {
-                let stack_buf = output.to_token_stream().to_string().replace("CPU", "Stack");
-                *output = insert_const_n_to_buf(stack_buf.to_token_stream(), output.clone());
+                *output = insert_const_n_to_buf(output.to_token_stream());
             }
-            
-            meth.sig.inputs = meth.sig.inputs.iter_mut().map(|input| {
-                match input.clone() {
-                    // self
-                    syn::FnArg::Receiver(_) => input.clone(),
-                    // other args
-                    syn::FnArg::Typed(typed) => {
-                        insert_const_n_to_buf(typed.to_token_stream(), input.clone())
-                    },
-                }
-            }).collect();
-            
+
+            meth.sig.inputs = meth
+                .sig
+                .inputs
+                .iter_mut()
+                .map(|input| {
+                    match input.clone() {
+                        // self
+                        syn::FnArg::Receiver(_) => input.clone(),
+                        // other args
+                        syn::FnArg::Typed(typed) => insert_const_n_to_buf(typed.to_token_stream()),
+                    }
+                })
+                .collect();
+
             acc.extend(meth.to_token_stream());
 
             acc
         });
 
-        panic!("methods: {}", methods_updated.to_token_stream().to_string());
+        //panic!("methods: {}", methods_updated.to_token_stream().to_string());
 
-        quote! {
-            impl<#generics, const N: usize> #trait_name<#generics> for custos::Stack
-            where custos::Stack: custos::Alloc<#generic_token_stream, N>
+        return quote! {
+            #impl_block
+
+            #attrs
+            impl<#spawn_generics, const N: usize> #trait_path for custos::stack::Stack
+            #where_clause
+            custos::stack::Stack: custos::Alloc<T, N>
             {
-
+                #methods_updated
             }
         };
+        //panic!("x: {}", x.to_string());
     }
 
     quote! {}
 }
 
-fn insert_const_n_to_buf<R: syn::parse::Parse + Clone>(tokens: proc_macro2::TokenStream, input: R) -> R {
-    let mut tokens = tokens.to_string();
+fn insert_const_n_to_buf<R: syn::parse::Parse + Clone>(tokens: proc_macro2::TokenStream) -> R {
+    let tokens = tokens.to_string();
     if !tokens.contains("Buffer") {
-        return input.clone();
+        return syn::parse_str(&tokens).unwrap();
     }
+    let mut tokens = tokens.replace("CPU", "Stack");
 
     let idx = tokens.find('>').unwrap();
-    tokens.insert_str(idx-1, ", N ");
-
+    tokens.insert_str(idx - 1, ", N ");
     syn::parse_str(&tokens).unwrap()
 }
